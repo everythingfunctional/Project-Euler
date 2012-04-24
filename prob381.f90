cae94e60f10c4e281de433e8b8da84b768d20b25
Brad Richardson <brad.richardson@tranware.net>
2012-04-24 13:53:55 -0500

    4-24-12

--- prob381.f90
+++ prob381.f90
@@ -1,44 +1,42 @@
 Program prob381
 Implicit Integer (Selected_Int_Kind(16)) (I-N)
-Common / iprimes / iprime(1:1000000),nprimes
-Save / iprimes /
-Call FindPrimes(10**8)
-!Do np = 3, nprimes
-!  nsum = 0
-!  Do k = 1, 5
-!    nfact = 1
-!    Do i = 2, np-k
-!      nfact = nfact*i
-!    End Do
-!    nsum = nsum+nfact
-!  End Do
-!  Print *, 'p = ',np
-!  Print *, 'Sum is ',nsum
-!  Print *, 'Mod is ',mod(nsum,np)
-!  nans = nans+mod(nsum,np)
-!End Do
-
-End Program prob381
-Subroutine FindPrimes (nEND)
-Implicit Integer (Selected_Int_Kind(16)) (I-N)
-Common / iprimes / iprime(1:1000000),nprimes
-Save / iprimes /
+Logical, Allocatable :: lprime(:)
+Integer :: iprime(1:1000000)
+nEND = 100
+Allocate (lprime(2:nEND))
+lprime(3:nEND:2) = .TRUE.
+lprime(2:nEND:2) = .FALSE.
 iprime(1) = 2
-iprime(2) = 3
-nprimes = 2
-n = 3
-Do
-  n = n+2
-  If ( n > nEND ) Exit
-  Do i = 2, nprimes
-    If ( mod(n,iprime(i)) == 0 ) Exit
-  End Do
-  If ( i > nprimes ) Then
+nprimes = 1
+Do i = 3, nEND/2, 2
+  If ( lprime(i) ) Then
+    lprime(2*i:nEND:i) = .FALSE.
     nprimes = nprimes+1
-    iprime(nprimes) = n
-!    Print *, 'Prime ',i,' is ',iprime(nprimes)
+    iprime(nprimes) = i
+    !Print *, 'Prime',nprimes,'is',i
   End If
 End Do
-Print *, 'Number of primes is',nprimes
-Return
-End Subroutine FindPrimes
+Do i = nEND/2+1, nEND, 2
+  If ( lprime(i) ) Then
+    nprimes = nprimes+1
+    iprime(nprimes) = i
+    !Print *, 'Prime',nprimes,'is',i
+  End If
+End Do
+!Print *, 'Number of primes is',nprimes
+Do np = 3, nprimes
+  nmod = 0
+  Do k = 1, 5
+    nfact = 1
+    Do j = iprime(np)-k, 2, -1
+      nfact = nfact*j
+      nfact = mod(nfact,iprime(np))
+      If ( nfact == 0 ) Exit
+    End Do
+    nmod = nmod+nfact
+    Print *, 'Funtion for prime',np,'=',nmod
+  End Do
+  nsum = nsum+mod(nmod,iprime(np))
+End Do
+Print *, 'Answer is ',nsum
+End Program prob381
