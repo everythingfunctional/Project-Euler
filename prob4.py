97d3e5b7ad21c506f7d58e694d6494e562141fe4
Brad Richardson <brad.richardson@tranware.net>
2012-08-08 13:18:15 -0500

    finished python problem 4

--- prob4.py
+++ prob4.py
@@ -1,4 +1,28 @@
-def palindrome_check(number)
-    length = log10(number) + 1
+from math import *
+
+def palindrome_check(number):
+    length = int(log10(number)) + 1
     check_number = length / 2
-    
+    digits = []
+    for i in reversed(range(length)):
+        digits.append(number / (10**i))
+        number -= (number / 10**i) * 10**i
+
+    for i in range(check_number):
+        if digits[i] != digits[-i-1]:
+            palindrome = False
+            break
+        else:
+            palindrome = True
+
+    return palindrome
+
+maximum_palindrome = 0
+
+for i in reversed(range(100,1000)):
+    for j in reversed(range(100,1000)):
+        product = i * j
+        if product > maximum_palindrome and palindrome_check(product):
+            maximum_palindrome = product
+
+print 'Maximum Palindrome is ',maximum_palindrome
