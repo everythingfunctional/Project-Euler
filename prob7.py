afb2921342509c7b27285b29d4f2825229cd119e
Brad Richardson <brad.richardson@tranware.net>
2012-08-20 13:33:57 -0500

    Finished python problem 7

--- prob7.py
+++ prob7.py
@@ -1,10 +1,31 @@
-end = int(10e8)
-is_prime = range(3, end+1, 2)
-is_prime.insert(0, 2)
+end = int(1000000)
+is_prime = [False]
+i = 0
+while i < end:
+    is_prime.append(True)
+    i += 1
+is_prime[1] = False
 
-for i in is_prime:
-    for i in xrange(2 * i, end, i):
-        is_prime.remove(i)
+i = 4
+while i <= end:
+    is_prime[i] = False
+    i += 2
+
+i = 3
+while i < end / 2:
+    if is_prime[i]:
+        j = i * 2
+        while j < end:
+            is_prime[j] = False
+            j += i
+    i += 2
+
+primes = [2]
+i = 3
+while i < end:
+    if is_prime[i]:
+        primes.append(i)
+    i += 2
 
 print 'Prime 6 is', primes[5]
-print 'Prime 10001 is', primes[10005]
+print 'Prime 10001 is', primes[10000]
